33263475051a66c061b965d6f1590cf0b1e0c465
ayush1300 <39218868+ayush1300@users.noreply.github.com>
2020-04-17 02:18:56 +0530

    Update Parser.py

--- Compiled/Parser.py
+++ Compiled/Parser.py
@@ -18,10 +18,16 @@ class Parser():
             return Statements(self.builder, self.module, p)
 
         @self.pg.production('statements : printstatement')
+        @self.pg.production('statements : assignmentstatement')
         @self.pg.production('statements : statements printstatement')
+        @self.pg.production('statements : statements assignmentstatement')
         def statements(p):
             return Line(self.builder, self.module, p)
 
+        @self.pg.production('assignmentstatement : VAR ASSIGN expression SEMI_COLON')
+        def assignmentstatement(p):
+            return Assign(p[0].value, p[2])
+        
         
         @self.pg.production('printstatement : PRINT OPEN_PAREN expression CLOSE_PAREN SEMI_COLON')
         def printstatement(p):
@@ -30,6 +36,9 @@ class Parser():
 
         @self.pg.production('expression : expression SUM expression')
         @self.pg.production('expression : expression SUB expression')
+        @self.pg.production('expression : expression MUL expression')
+        @self.pg.production('expression : expression DIV expression')
+        @self.pg.production('expression : expression MOD expression')
         def expression(p):
             left = p[0]
             right = p[2]
@@ -38,10 +47,21 @@ class Parser():
                 return Sum(self.builder, self.module, left, right)
             elif operator.gettokentype() == 'SUB':
                 return Sub(self.builder, self.module, left, right)
+            elif operator.gettokentype() == 'MUL':
+                return Mul(left, right)
+            elif operator.gettokentype() == 'DIV':
+                return Div(left, right)
+            elif operator.gettokentype() == 'MOD':
+                return Mod(left, right)
+            
 
         @self.pg.production('expression : NUMBER')
         def number(p):
             return Number(self.builder, self.module, p[0].value)
+        
+        @self.pg.production('expression : VAR')
+        def number(p):
+            return Var(p[0].value)
 
         @self.pg.error
         def error_handle(token):
